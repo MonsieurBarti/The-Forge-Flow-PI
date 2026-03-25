93c6393b9bcafed04a6ee7ce710a4a30fb13dbc6
MonsieurBarti <github@barti.mozmail.com>
2026-03-25 19:36:49 +0100

    feat(S06/T09): add SqliteMilestoneRepository stub

---
+++ src/hexagons/milestone/infrastructure/sqlite-milestone.repository.ts
@@ -0,0 +1,21 @@
+import type { Id, PersistenceError, Result } from "@kernel";
+import type { Milestone } from "../domain/milestone.aggregate";
+import { MilestoneRepositoryPort } from "../domain/milestone-repository.port";
+
+export class SqliteMilestoneRepository extends MilestoneRepositoryPort {
+  save(_milestone: Milestone): Promise<Result<void, PersistenceError>> {
+    throw new Error("Not implemented");
+  }
+
+  findById(_id: Id): Promise<Result<Milestone | null, PersistenceError>> {
+    throw new Error("Not implemented");
+  }
+
+  findByLabel(_label: string): Promise<Result<Milestone | null, PersistenceError>> {
+    throw new Error("Not implemented");
+  }
+
+  findByProjectId(_projectId: Id): Promise<Result<Milestone[], PersistenceError>> {
+    throw new Error("Not implemented");
+  }
+}
