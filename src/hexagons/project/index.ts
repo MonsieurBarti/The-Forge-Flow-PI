d801da7d13e6bb624a7e6c8dc9761d20205acefb
MonsieurBarti <github@barti.mozmail.com>
2026-03-25 18:52:33 +0100

    feat(S05/T10): add project hexagon barrel export

---
+++ src/hexagons/project/index.ts
@@ -0,0 +1,4 @@
+export type { ProjectDTO } from "./domain/project.schemas";
+export { ProjectPropsSchema } from "./domain/project.schemas";
+export { ProjectInitializedEvent } from "./domain/project-initialized.event";
+export { ProjectRepositoryPort } from "./domain/project-repository.port";
