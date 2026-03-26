8cb7be9469399a5f981343b9b2e063ca51ff9435
MonsieurBarti <github@barti.mozmail.com>
2026-03-26 15:34:08 +0100

    feat(S05/T05): add barrel exports and verify git CLI adapter

--- src/kernel/infrastructure/index.ts
+++ src/kernel/infrastructure/index.ts
@@ -1,3 +1,4 @@
 export { ConsoleLoggerAdapter } from "./console-logger.adapter";
+export { GitCliAdapter } from "./git-cli.adapter";
 export { InProcessEventBus } from "./in-process-event-bus";
 export { SilentLoggerAdapter } from "./silent-logger.adapter";
